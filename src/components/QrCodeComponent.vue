30326bb3c9722e4319f8cd987eb081061c3a5a08
Cornelio Jr. <52844136+Cornelio-II@users.noreply.github.com>
2025-03-25 17:05:11 +0800

    Update QrCodeComponent.vue

--- src/components/QrCodeComponent.vue
+++ src/components/QrCodeComponent.vue
@@ -3,7 +3,7 @@ import imageQR from '@/assets/images/image-qr-code.png'
 </script>
 <template>
   <div class="container">
-    <img :src="imageQR" alt="" />
+    <img :src="imageQR" alt="image QR code" />
     <h1>Improve your front-end skills by building projects</h1>
     <p>Scan the QR code to visit Frontend Mentor and take your coding skills to the next level</p>
   </div>
