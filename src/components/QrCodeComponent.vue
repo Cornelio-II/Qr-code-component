ff3273ceed9dc5960e53dce8b5449dfcb950b570
@Cornelio <jun23.llagas@gmail.com>
2025-05-30 18:30:02 +0800

    Style: updating clean codes, and remove uncessary syntax

--- src/components/QrCodeComponent.vue
+++ src/components/QrCodeComponent.vue
@@ -6,7 +6,10 @@ import imageQR from '@/assets/images/image-qr-code.png'
     <article>
     <div class="container">
     <header>
-      <img :src="imageQR" alt="image QR code" />
+      <div class="wrapper">
+           <img :src="imageQR" alt="image QR code" />
+      </div>
+
       <h1>Improve your front-end skills by building projects</h1>
       <p>Scan the QR code to visit Frontend Mentor and take your coding skills to the next level</p>
     </header>
