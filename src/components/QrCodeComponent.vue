cac29986d9f370234f1fef686b683427ecd85638
@Cornelio <jun23.llagas@gmail.com>
2025-06-03 20:33:13 +0800

    Style: updating clean codes, and remove uncessary syntax

--- src/components/QrCodeComponent.vue
+++ src/components/QrCodeComponent.vue
@@ -2,24 +2,20 @@
 import imageQR from '@/assets/images/image-qr-code.png'
 </script>
 <template>
-  <main>
-    <article>
-    <div class="container">
-    <header>
-      
-      <img :src="imageQR" alt="image QR code" />
-
-      <h1>Improve your front-end skills by building projects</h1>
-      <p>Scan the QR code to visit Frontend Mentor and take your coding skills to the next level</p>
-    </header>
-  </div>
+    <main>
+      <header>
+        <img :src="imageQR" alt="image QR code" />
+      </header>
+      <section>
+        <h1>Improve your front-end skills by building projects</h1>
+        <p>Scan the QR code to visit Frontend Mentor and take your coding skills to the next level</p>
+      </section>
+    </main>
    <footer>
-      <div class="attribution">
+     <div class="attribution" role="contentinfo">
       Challenge by
       <a href="https://www.frontendmentor.io?ref=challenge" target="_blank">Frontend Mentor</a>. Coded
       by <a href="https://github.com/cornelio-ii/Qr-code-component" target="_blank">Cornelio Llagas</a>.
     </div>
   </footer>
-  </article>
-  </main>
 </template>
